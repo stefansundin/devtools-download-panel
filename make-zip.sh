498a98c0d9984ffa8958e51f1908607e1c55ef16
Stefan Sundin <git@stefansundin.com>
2015-11-01 18:41:46 -0800

    Update compilation scripts.

--- make-zip.sh
+++ make-zip.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -ex
 V=$(cat download-panel/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
-rm -f "download-panel-$V.zip"
-zip -r "download-panel-$V.zip" download-panel/*
+rm -f "download-panel-v$V.zip"
+zip -r "download-panel-v$V.zip" download-panel/*
