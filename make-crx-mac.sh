32c172eb15d04878940c78e6aa28fc077f452eb4
Stefan Sundin <git@stefansundin.com>
2017-08-15 13:45:12 -0700

    New bash scripts using jq.

--- make-crx-mac.sh
+++ make-crx-mac.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 WD=$(pwd)
-V=$(cat download-panel/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
+V=$(cat download-panel/manifest.json | jq -Mr .version)
 
 "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome" --pack-extension="$WD/download-panel" --pack-extension-key="$WD/download-panel.pem"
 
-mv download-panel.crx "download-panel-v$V.crx"
+mv download-panel.crx "download-panel-$V.crx"
--- make-zip.sh
+++ make-zip.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -ex
-V=$(cat download-panel/manifest.json | grep '"version"' | grep -o "\d*\.\d*\.\d*")
-rm -f "download-panel-v$V.zip"
-zip -r "download-panel-v$V.zip" download-panel/*
+V=$(cat download-panel/manifest.json | jq -Mr .version)
+rm -f "download-panel-$V.zip"
+cd download-panel
+zip -r "../download-panel-$V.zip" . -x '*.DS_Store' -x '*Thumbs.db'
