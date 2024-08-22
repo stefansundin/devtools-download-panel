8921357792440ebc1b24dec9aa356aa86cab5b4c
Stefan Sundin <git@stefansundin.com>
2024-08-21 20:10:21 -0700

    Rename directory.

--- make-zip.sh
+++ make-zip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 mkdir -p dist
-V=$(cat download-panel/manifest.json | jq -Mr .version)
+V=$(cat chrome/manifest.json | jq -Mr .version)
 rm -f "download-panel-$V.zip"
-cd download-panel
+cd chrome
 zip -r "../dist/download-panel-$V.zip" . -x '*.DS_Store' -x '*Thumbs.db'
